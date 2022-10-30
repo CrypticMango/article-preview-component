27de990b06f4ce9b014378479a3a73c8068cb314
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-10-30 11:07:18 -0600

    added function for showing the panel

---
+++ app.js
@@ -0,0 +1,10 @@
+const panel = document.getElementById("panel");
+const shareButton = document.getElementById("share-button-main");
+const shareButtonPanel = document.getElementById("share-button-panel");
+
+shareButton.addEventListener("click", showPanel);
+shareButtonPanel.addEventListener("click", showMichelle);
+
+function showPanel() {
+    panel.style.display = "flex"
+}
