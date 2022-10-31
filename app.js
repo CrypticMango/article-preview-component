af992a9115fe1df702a08fbfa8466d46b135e69e
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-10-31 06:24:57 -0600

    styled the share panel for desktop view
    -added function for hiding share panel on desktop

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const panel = document.getElementById("panel");
 const shareButton = document.getElementById("share-button-main");
 const shareButtonPanel = document.getElementById("share-button-panel");
+const shareWord = document.getElementById("share-word");
 
 shareButton.addEventListener("click", showPanel);
 shareButtonPanel.addEventListener("click", showMichelle);
+shareWord.addEventListener("click", hideShareClick);
 
 function showPanel() {
     panel.style.display = "flex";
 }
 
 function showMichelle() {
+    panel.style.display = "none";
+}
+
+function hideShareClick() {
     panel.style.display = "none";
 }
